b2c32217f881215e6b05a5afb516fe0e9432c3f5
Sameep1994 <63553538+Sameep1994@users.noreply.github.com>
2021-01-06 22:40:48 +0530

    queue and dequeue code

--- src/main/java/MaxOfSubArrOfSizeK.java
+++ src/main/java/MaxOfSubArrOfSizeK.java
@@ -6,6 +6,19 @@ public class MaxOfSubArrOfSizeK {
         int a[]={20,40,30,10,60};
         maxSub(a,3);
     }
+    /*Insert an element in dq if a element greater than
+    * front of dq we empty dq and add the element in front of dq
+    * when either dq of size k is completly filled or
+    * we find a greater element than front we put the front element of dq in result
+    *
+    * */
+
+
+
+
+
+
+
 //we need to find the max element in every subarray of size k
 //we will use dequeue and all all the small element and whenever encounter a largest element we will remove all from dequeue
     public static void maxSub(int[] a,int k){
@@ -21,7 +34,7 @@ public class MaxOfSubArrOfSizeK {
         for (int i=k;i<a.length;i++){
             //since the beginning of the dq always contains the largest element
             System.out.println(a[dq.peekFirst()]);
-            //this part makes sure the current window gets processed,peek checks from the front
+            //this part makes sure the current window gets processed,peek checks from the front(remove elements not part of current window)
             while (dq.isEmpty()== false && dq.peekFirst() <=i-k)
                 dq.removeFirst();
             //remove the samllest element from dq
@@ -29,6 +42,7 @@ public class MaxOfSubArrOfSizeK {
                 dq.removeLast();
             dq.addLast(i);
         }
+        //print for last window
         System.out.println(a[dq.peekFirst()]);
     }
 }
